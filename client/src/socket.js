c0ebba177e92d5519a74606a3567817ecd9e5e8f
SaidBC <razzouksaid139@gmail.com>
2024-12-13 03:43:31 +0100

    test url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.BASE_URL || "http://localhost:8000";
+const URL = "https://tic-tac-toe-online-production.up.railway.app/";
 
 export const socket = io(URL, {
   autoConnect: false,
