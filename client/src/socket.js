3f61b836cccf5390883fda69594baa80283eab42
SaidBC <razzouksaid139@gmail.com>
2025-01-18 04:36:53 +0100

    Refactor socket connection URL to use environment variable

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = "https://tic-tac-toe-online-production.up.railway.app/";
+const URL = import.meta.env.VITE_SERVER_URL;
 
 export const socket = io(URL, {
   autoConnect: false,
